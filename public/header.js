a2c605b737d21d24fc96f9cc2bd40be7ef00beda
Brooke Thompson <brookethompson0341@gmail.com>
2024-12-11 22:07:50 -0500

    Update header.js

--- public/header.js
+++ public/header.js
@@ -1,71 +1,153 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var header = document.createElement('header');
-    header.innerHTML = `
-        <div class="top" id="top">
-            <!-- Secondary navigation bar -->
-            <div class="secondary-navigation">
-                <div style="min-width: 10px;"></div>
-                <div class="navbar">
-                    <ul>
-                        <li>
-                            <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search1"> <!-- May change page name -->
-                                <input type="text" placeholder="Search books" name="search">
-                                <button type="submit"><i class="fa fa-search"></i></button>
-                            </form>
-                        </li>
-                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/subscription.html">Subscriptions</a></li>
-                        <li><strong><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/login.html" id="loginProfile1">Log In</a></strong></li>
-                    </ul>
+    const headerContainer = document.createElement('header');
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser) {
+        headerContainer.innerHTML =`
+            <div class="top" id="top">
+                <!-- Secondary navigation bar -->
+                <div class="secondary-navigation">
+                    <div style="min-width: 10px;"></div>
+                    <div class="navbar">
+                        <ul>
+                            <li>
+                                <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search1"> <!-- May change page name -->
+                                    <input type="text" placeholder="Search books" name="search">
+                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </li>
+                            <li><a href="subscription.html">Subscriptions</a></li>
+                            <li><strong><a href="my-profile.html" id="loginProfile1">Profile</a></strong></li>
+                        </ul>
+                    </div>
+                    <div style="min-width: 10px"></div>
                 </div>
-                <div style="min-width: 10px"></div>
-            </div>
-            <!-- Navigation bar -->
-            <div class="header">
-                <div style="min-width: 10px;"></div>
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/home.html"><img src="/logo.png" alt="Logo"></a>
-                <div></div>
-                <div class="navbar">
-                    <ul>
-                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/about.html">About</a></li>
-                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/map.html">SwapSpots</a></li>
-                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/faq.html">FAQ</a></li>
-                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/contact.html">Contact</a></li>
-                    </ul>
+                // Add logout functionality
+                headerContainer.querySelector('#logout-link').addEventListener('click', function(e) {
+                    e.preventDefault();
+                    localStorage.removeItem('currentUser');
+                    window.location.href = 'login.html';
+                });
+                <!-- Navigation bar -->
+                <div class="header">
+                    <div style="min-width: 10px;"></div>
+                    <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/home.html"><img src="/logo.png" alt="Logo"></a>
+                    <div></div>
+                    <div class="navbar">
+                        <ul>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="map.html">SwapSpots</a></li>
+                            <li><a href="faq.html">FAQ</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="hamburger"> <!-- Hamburger menu -->
+                        <a href="javascript:void(0);" class="icon" onclick="toggle()">
+                                <i class="fa fa-bars"></i>
+                        </a>
+                    </div>
+                    <div style="min-width: 10px"></div>
                 </div>
-                <div class="hamburger"> <!-- Hamburger menu -->
-                    <a href="javascript:void(0);" class="icon" onclick="toggle()">
-                            <i class="fa fa-bars"></i>
-                    </a>
+                
+                <!-- Expandable menu -->
+                <div class="menu" id="menu">
+                    <a href="about.html">About</a>
+                    <br>
+                    <a href="map.html">SwapSpots</a>
+                    <br>
+                    <a href="faq.html">FAQ</a>
+                    <br>
+                    <a href="contact.html">Contact</a>
+                    <br>
+                    <a href="subscription.html">Subscriptions</a>
+                    <br>
+                    <a href="my-profile.html" id="Profile"></a>
+                    <br>
+                    <li><a href="#" id="logout-link">Log Out</a></li>
+                    <br>
+                    <form action="find.html" id="search2"> <!-- May change page name -->
+                        <input type="text" placeholder="Search books" name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
                 </div>
-                <div style="min-width: 10px"></div>
             </div>
-            
-            <!-- Expandable menu -->
-            <div class="menu" id="menu">
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/about.html">About</a>
-                <br>
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/map.html">SwapSpots</a>
-                <br>
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/faq.html">FAQ</a>
-                <br>
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/contact.html">Contact</a>
-                <br>
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/subscription.html">Subscriptions</a>
-                <br>
-                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/login.html" id="loginProfile2"></a>
-                <br>
-                <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search2"> <!-- May change page name -->
-                    <input type="text" placeholder="Search books" name="search">
-                    <button type="submit"><i class="fa fa-search"></i></button>
-                </form>
-            </div>
-        </div>
 
-        <!-- Space under navigation bar -->
-        <div class="blank-space"></div>
-    `;
-    document.body.insertBefore(header, document.body.firstChild);
-});
+            <!-- Space under navigation bar -->
+            <div class="blank-space"></div>
+        `;
+        document.body.insertBefore(header, document.body.firstChild);
+        } else {
+            headerContainer.innerHTML =`
+            <div class="top" id="top">
+                <!-- Secondary navigation bar -->
+                <div class="secondary-navigation">
+                    <div style="min-width: 10px;"></div>
+                    <div class="navbar">
+                        <ul>
+                            <li>
+                                <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search1"> <!-- May change page name -->
+                                    <input type="text" placeholder="Search books" name="search">
+                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </li>
+                            <li><a href="subscription.html">Subscriptions</a></li>
+                            <li><strong><a href="my-profile.html" id="loginProfile1">Profile</a></strong></li>
+                        </ul>
+                    </div>
+                    <div style="min-width: 10px"></div>
+                </div>
+                // Add logout functionality
+                headerContainer.querySelector('#logout-link').addEventListener('click', function(e) {
+                    e.preventDefault();
+                    localStorage.removeItem('currentUser');
+                    window.location.href = 'login.html';
+                });
+                <!-- Navigation bar -->
+                <div class="header">
+                    <div style="min-width: 10px;"></div>
+                    <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/home.html"><img src="/logo.png" alt="Logo"></a>
+                    <div></div>
+                    <div class="navbar">
+                        <ul>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="map.html">SwapSpots</a></li>
+                            <li><a href="faq.html">FAQ</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="hamburger"> <!-- Hamburger menu -->
+                        <a href="javascript:void(0);" class="icon" onclick="toggle()">
+                                <i class="fa fa-bars"></i>
+                        </a>
+                    </div>
+                    <div style="min-width: 10px"></div>
+                </div>
+                
+                <!-- Expandable menu -->
+                <div class="menu" id="menu">
+                    <a href="about.html">About</a>
+                    <br>
+                    <a href="map.html">SwapSpots</a>
+                    <br>
+                    <a href="faq.html">FAQ</a>
+                    <br>
+                    <a href="contact.html">Contact</a>
+                    <br>
+                    <a href="subscription.html">Subscriptions</a>
+                    <br>
+                    <a href="login.html.html" id="Login"></a>
+                    <br>
+                    <form action="find.html" id="search2"> <!-- May change page name -->
+                        <input type="text" placeholder="Search books" name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Space under navigation bar -->
+            <div class="blank-space"></div>
+        `;
+        document.body.insertBefore(header, document.body.firstChild);
+    }});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Show or hide search bar, depending on page
