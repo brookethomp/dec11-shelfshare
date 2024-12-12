c4651e13de44ca905f559c868ed7ae39d9f981d1
Brooke Thompson <brookethompson0341@gmail.com>
2024-12-11 22:11:08 -0500

    Update header.js

--- public/header.js
+++ public/header.js
@@ -1,153 +1,71 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const headerContainer = document.createElement('header');
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    if (currentUser) {
-        headerContainer.innerHTML =`
-            <div class="top" id="top">
-                <!-- Secondary navigation bar -->
-                <div class="secondary-navigation">
-                    <div style="min-width: 10px;"></div>
-                    <div class="navbar">
-                        <ul>
-                            <li>
-                                <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search1"> <!-- May change page name -->
-                                    <input type="text" placeholder="Search books" name="search">
-                                    <button type="submit"><i class="fa fa-search"></i></button>
-                                </form>
-                            </li>
-                            <li><a href="subscription.html">Subscriptions</a></li>
-                            <li><strong><a href="my-profile.html" id="loginProfile1">Profile</a></strong></li>
-                        </ul>
-                    </div>
-                    <div style="min-width: 10px"></div>
+    var header = document.createElement('header');
+    header.innerHTML = `
+        <div class="top" id="top">
+            <!-- Secondary navigation bar -->
+            <div class="secondary-navigation">
+                <div style="min-width: 10px;"></div>
+                <div class="navbar">
+                    <ul>
+                        <li>
+                            <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search1"> <!-- May change page name -->
+                                <input type="text" placeholder="Search books" name="search">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
+                        </li>
+                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/subscription.html">Subscriptions</a></li>
+                        <li><strong><a href="#" id="loginProfile1">Log In</a></strong></li>
+                    </ul>
                 </div>
-                // Add logout functionality
-                headerContainer.querySelector('#logout-link').addEventListener('click', function(e) {
-                    e.preventDefault();
-                    localStorage.removeItem('currentUser');
-                    window.location.href = 'login.html';
-                });
-                <!-- Navigation bar -->
-                <div class="header">
-                    <div style="min-width: 10px;"></div>
-                    <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/home.html"><img src="/logo.png" alt="Logo"></a>
-                    <div></div>
-                    <div class="navbar">
-                        <ul>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="map.html">SwapSpots</a></li>
-                            <li><a href="faq.html">FAQ</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div class="hamburger"> <!-- Hamburger menu -->
-                        <a href="javascript:void(0);" class="icon" onclick="toggle()">
-                                <i class="fa fa-bars"></i>
-                        </a>
-                    </div>
-                    <div style="min-width: 10px"></div>
-                </div>
-                
-                <!-- Expandable menu -->
-                <div class="menu" id="menu">
-                    <a href="about.html">About</a>
-                    <br>
-                    <a href="map.html">SwapSpots</a>
-                    <br>
-                    <a href="faq.html">FAQ</a>
-                    <br>
-                    <a href="contact.html">Contact</a>
-                    <br>
-                    <a href="subscription.html">Subscriptions</a>
-                    <br>
-                    <a href="my-profile.html" id="Profile"></a>
-                    <br>
-                    <li><a href="#" id="logout-link">Log Out</a></li>
-                    <br>
-                    <form action="find.html" id="search2"> <!-- May change page name -->
-                        <input type="text" placeholder="Search books" name="search">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
+                <div style="min-width: 10px"></div>
             </div>
-
-            <!-- Space under navigation bar -->
-            <div class="blank-space"></div>
-        `;
-        document.body.insertBefore(header, document.body.firstChild);
-        } else {
-            headerContainer.innerHTML =`
-            <div class="top" id="top">
-                <!-- Secondary navigation bar -->
-                <div class="secondary-navigation">
-                    <div style="min-width: 10px;"></div>
-                    <div class="navbar">
-                        <ul>
-                            <li>
-                                <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search1"> <!-- May change page name -->
-                                    <input type="text" placeholder="Search books" name="search">
-                                    <button type="submit"><i class="fa fa-search"></i></button>
-                                </form>
-                            </li>
-                            <li><a href="subscription.html">Subscriptions</a></li>
-                            <li><strong><a href="my-profile.html" id="loginProfile1">Profile</a></strong></li>
-                        </ul>
-                    </div>
-                    <div style="min-width: 10px"></div>
+            <!-- Navigation bar -->
+            <div class="header">
+                <div style="min-width: 10px;"></div>
+                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/home.html"><img src="/logo.png" alt="Logo"></a>
+                <div></div>
+                <div class="navbar">
+                    <ul>
+                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/about.html">About</a></li>
+                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/map.html">SwapSpots</a></li>
+                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/faq.html">FAQ</a></li>
+                        <li><a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/contact.html">Contact</a></li>
+                    </ul>
                 </div>
-                // Add logout functionality
-                headerContainer.querySelector('#logout-link').addEventListener('click', function(e) {
-                    e.preventDefault();
-                    localStorage.removeItem('currentUser');
-                    window.location.href = 'login.html';
-                });
-                <!-- Navigation bar -->
-                <div class="header">
-                    <div style="min-width: 10px;"></div>
-                    <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/home.html"><img src="/logo.png" alt="Logo"></a>
-                    <div></div>
-                    <div class="navbar">
-                        <ul>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="map.html">SwapSpots</a></li>
-                            <li><a href="faq.html">FAQ</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div class="hamburger"> <!-- Hamburger menu -->
-                        <a href="javascript:void(0);" class="icon" onclick="toggle()">
-                                <i class="fa fa-bars"></i>
-                        </a>
-                    </div>
-                    <div style="min-width: 10px"></div>
+                <div class="hamburger"> <!-- Hamburger menu -->
+                    <a href="javascript:void(0);" class="icon" onclick="toggle()">
+                            <i class="fa fa-bars"></i>
+                    </a>
                 </div>
-                
-                <!-- Expandable menu -->
-                <div class="menu" id="menu">
-                    <a href="about.html">About</a>
-                    <br>
-                    <a href="map.html">SwapSpots</a>
-                    <br>
-                    <a href="faq.html">FAQ</a>
-                    <br>
-                    <a href="contact.html">Contact</a>
-                    <br>
-                    <a href="subscription.html">Subscriptions</a>
-                    <br>
-                    <a href="login.html.html" id="Login"></a>
-                    <br>
-                    <form action="find.html" id="search2"> <!-- May change page name -->
-                        <input type="text" placeholder="Search books" name="search">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </form>
-                </div>
+                <div style="min-width: 10px"></div>
             </div>
+            
+            <!-- Expandable menu -->
+            <div class="menu" id="menu">
+                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/about.html">About</a>
+                <br>
+                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/map.html">SwapSpots</a>
+                <br>
+                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/faq.html">FAQ</a>
+                <br>
+                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/contact.html">Contact</a>
+                <br>
+                <a href="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/subscription.html">Subscriptions</a>
+                <br>
+                <a href="#" id="loginProfile2"></a>
+                <br>
+                <form action="https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/find.html" id="search2"> <!-- May change page name -->
+                    <input type="text" placeholder="Search books" name="search">
+                    <button type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div>
 
-            <!-- Space under navigation bar -->
-            <div class="blank-space"></div>
-        `;
-        document.body.insertBefore(header, document.body.firstChild);
-    }});
+        <!-- Space under navigation bar -->
+        <div class="blank-space"></div>
+    `;
+    document.body.insertBefore(header, document.body.firstChild);
+});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Show or hide search bar, depending on page
@@ -163,11 +81,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // Check if user is logged in
     if (!currentUser) {
         document.getElementById('loginProfile1').innerText = "Log In";
+        document.getElementById('loginProfile1').href = "https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/login.html";
         document.getElementById('loginProfile2').innerText = "Log In";
+        document.getElementById('loginProfile2').href = "https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/login.html";
     }
     else { 
         document.getElementById('loginProfile1').innerText = "Profile";
+        document.getElementById('loginProfile1').href = "https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/my-profile.html";
         document.getElementById('loginProfile2').innerText = "Profile";
+        document.getElementById('loginProfile2').href = "https://dec11-shelfshare-d2a5bdd1bfe2.herokuapp.com/my-profile.html";
     }
 });
 
